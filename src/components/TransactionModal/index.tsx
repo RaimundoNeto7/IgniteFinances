470a005d9cdfa28ba37f8ce9927d703bf1504a98
Raimundo Neto <raimundoneto.rn17@gmail.com>
2021-07-11 19:52:36 -0400

    Add container to choose types of transactions

--- src/components/TransactionModal/index.tsx
+++ src/components/TransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal'
-import { Container } from './styles'
+import { Container, TransactionTypeContainer } from './styles'
 import closeImg from '../../assets/close.svg'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
 
 Modal.setAppElement("#root")
 
@@ -29,6 +31,19 @@ export function TransactionModal({ isOpen, onRequestClose }: TransactionModalPro
                 <input placeholder="Título"/>
 
                 <input type="number" placeholder="Valor"/>
+
+                <TransactionTypeContainer>
+                    <button type="button">
+                        <img src={incomeImg} alt="Entrada"/>
+                        <span>Entrada</span>
+                    </button>
+
+                    <button type="button">
+                        <img src={outcomeImg} alt="Saída"/>
+                        <span>Saída</span>
+                    </button>
+                </TransactionTypeContainer>
+
                 <input placeholder="Categoria"/>
                 <button type="submit">Cadastrar</button>
             </Container>
