10d367007665b8aa26320abe5cfc0862ecf1262c
Raimundo Neto <raimundoneto.rn17@gmail.com>
2021-07-22 10:40:39 -0400

    Get input data and send to api

--- src/components/TransactionModal/index.tsx
+++ src/components/TransactionModal/index.tsx
@@ -3,7 +3,8 @@ import { Container, TransactionTypeContainer, TransactionTypeButton } from './st
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
+import { api } from '../../services/api'
 
 Modal.setAppElement("#root")
 
@@ -13,7 +14,23 @@ interface TransactionModalProps {
 }
 
 export function TransactionModal({ isOpen, onRequestClose }: TransactionModalProps) {
+    const [title, setTitle] = useState("")
+    const [value, setValue] = useState(0)
+    const [category, setCategory] = useState("")
     const [transactionType, setTransactionType] = useState("deposit")
+
+    function handleCreateTransaction(event: FormEvent) {
+        event.preventDefault()
+
+        const data = {
+            title,
+            value,
+            category,
+            transactionType
+        }
+
+        api.post("transactions", data)
+    }
 
     return (
         <Modal
@@ -28,12 +45,12 @@ export function TransactionModal({ isOpen, onRequestClose }: TransactionModalPro
                 className="react-modal-close">
                 <img src={closeImg} alt="Fechar modal"/>
             </button>
-            <Container>
+            <Container onSubmit={handleCreateTransaction}>
                 <h2>Cadastrar transação</h2>
 
-                <input placeholder="Título"/>
+                <input placeholder="Título" value={title} onChange={(event) => setTitle(event.target.value)}/>
 
-                <input type="number" placeholder="Valor"/>
+                <input type="number" value={value} placeholder="Valor" onChange={(event) => setValue(Number(event.target.value))}/>
 
                 <TransactionTypeContainer>
                     <TransactionTypeButton type="button" onClick={() => { setTransactionType("deposit") }} isActive={transactionType === "deposit"} activeColor="#b6ffe4">
@@ -47,7 +64,7 @@ export function TransactionModal({ isOpen, onRequestClose }: TransactionModalPro
                     </TransactionTypeButton>
                 </TransactionTypeContainer>
 
-                <input placeholder="Categoria"/>
+                <input placeholder="Categoria" value={category} onChange={(event) => setCategory(event.target.value)}/>
                 <button type="submit">Cadastrar</button>
             </Container>
         </Modal>
